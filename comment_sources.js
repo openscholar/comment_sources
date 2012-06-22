3eb108bf92a589b0e14e238a9fd047c566532334
Jeffrey Licht <jeffrey.licht@gmail.com>
2012-06-22 09:15:22 -0400

    Track facebook comments to support recent_comments api function

--- comment_sources.js
+++ comment_sources.js
@@ -3,11 +3,10 @@
  * JavaScript for the comment_sources module.
  */
 
-
 function disqus_track_comments(params){
   jQuery.ajax({
       type: 'GET',
-      url: 'http://localhost/~jlicht/os7/comment_sources/record_comment',
+      url: Drupal.settings.basePath + 'comment_sources/record_comment',
       data :
         {
           nid : Drupal.settings.comment_sources.nid,
@@ -17,4 +16,30 @@ function disqus_track_comments(params){
         }
   });
 }
+
+/**
+ * Retrieve the comment text from Facebook, and then save the comment. Requires page to be accessible by
+ * Facebook to retrieve comment text.
+ * @param params
+ */
+function fb_social_track_comments(params){
+  var commentQuery = FB.Data.query("SELECT text FROM comment WHERE post_fbid='" + params.commentID +
+      "' AND object_id IN (SELECT comments_fbid FROM link_stat WHERE url='" + params.href + "')");
+  FB.Data.waitOn([commentQuery], function () {
+    var commentRow = commentQuery.value[0];
+    jQuery.ajax({
+      type: 'GET',
+      url: Drupal.settings.basePath + 'comment_sources/record_comment',
+      data :
+        {
+          nid : Drupal.settings.comment_sources.nid,
+          source: 'fb_social',
+          external_id: params.commentID,
+          subject: (commentRow != undefined) ? commentRow.text.substring(0,64) : ''
+        }
+    });
+  });
+}
+
+
 
